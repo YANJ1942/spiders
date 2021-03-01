e896e9a30de917fbe555abaa2d338c94fd16fd3f
YANJ <karljiu1942@gmail.com>
2021-03-01 22:37:50 +0800

    I have solved the problem successfully. And this is the first picture.

--- Get_the_first_picture/get_the_first_picture.py
+++ Get_the_first_picture/get_the_first_picture.py
@@ -1,23 +1,23 @@
 import requests
 import os
 
-p = "I:\python\spiders\Get_the_first_picture\picture"
-url = "https://big.diercun.com/hd2/2021/0227/26/24mnorg_1 9.jpg"
-root = "/picture/"
+url = "https://img.tupianzj.com/uploads/allimg/180528/9-1P52Q94401.jpg"
+root = "/python/spiders/Get_the_first_picture/"
 path = root + url.split("/")[-1]
 try:
-    #whether the picture already exists
+    # 判断文件是否存在
     if not os.path.exists(root):
         os.mkdir(root)
     if not os.path.exists(path):
         r = requests.get(url)
-        #check the internet
+        # 判断网络连接的状态
         r.raise_for_status()
-        #we use the "with" format to avoid to close the file by ourselves
-        with open(p,"wb")as f:
+        #使用with语句可以不用自己手动关闭已经打开的文件流
+        with open('001.jpg' ,"wb") as f:
+            # 返回的是bytes型也就是二进制的数据
             f.write(r.content)
-        print("succeeded")
+        print("爬取完成")
     else:
-        print("the picture already exists")
+        print("文件已存在")
 except Exception as e:
-    print("failed")
+    print("爬取失败:"+str(e))
